caf8b1e84690ca776fb83c600fbf9d182c412ea9
ParkMarie <park_marie@mail.ru>
2024-04-13 18:31:52 +0300

    Добавьте файлы проекта.

---
+++ PraktikaEF/Page4.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PraktikaEF
+{
+    /// <summary>
+    /// Логика взаимодействия для Page4.xaml
+    /// </summary>
+    public partial class Page4 : Page
+    {
+        private BookStoreEntities context = new BookStoreEntities();
+        public Page4()
+        {
+            InitializeComponent();
+            OrderInfoDataGrid.ItemsSource = context.OrderInfo.ToList();
+        }
+    }
+}
